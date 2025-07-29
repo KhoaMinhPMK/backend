23c5589eeb05e5ddcda78b9ba3a2ac3d65c567a0
KhoaminhPMK <pmkkhoaminh@gmail.com>
2025-07-29 13:03:53 +0700

    a

--- sql/add_blood_pressure_fields.sql
+++ sql/add_blood_pressure_fields.sql
@@ -1,9 +1,9 @@
 -- Thêm các trường thông tin huyết áp vào bảng user
 ALTER TABLE user 
-ADD COLUMN blood_pressure_systolic INT NULL COMMENT 'Huyết áp tâm thu (mmHg)',
-ADD COLUMN blood_pressure_diastolic INT NULL COMMENT 'Huyết áp tâm trương (mmHg)', 
-ADD COLUMN heart_rate INT NULL COMMENT 'Nhịp tim (bpm)',
-ADD COLUMN last_health_check TIMESTAMP NULL COMMENT 'Thời gian kiểm tra sức khỏe cuối cùng';
+ADD COLUMN blood_pressure_systolic INT NULL,
+ADD COLUMN blood_pressure_diastolic INT NULL,
+ADD COLUMN heart_rate INT NULL,
+ADD COLUMN last_health_check TIMESTAMP NULL;
 
 -- Tạo index để tối ưu truy vấn
 CREATE INDEX idx_user_health_check ON user(last_health_check);
